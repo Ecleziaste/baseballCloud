ffb07eb13a219abee93f552a3fa9f8501dc82a38
Андрей <Andrej@MacBook-Pro-Alexey.local>
2021-07-01 16:16:16 +0600

    loading screen added to Profile component

--- src/routes/UserRouter/Profile/Profile.tsx
+++ src/routes/UserRouter/Profile/Profile.tsx
@@ -21,6 +21,7 @@ import { selectHeaders } from "../../../store/user/selectors";
 import { http } from "../../../services/http";
 
 const Profile: React.FC<Props> = () => {
+  const [spinner, setSpinner] = useState(true);
   const headers = useSelector(selectHeaders)!;
   http.setAuthorizationHeader(headers);
   const dispatch = useDispatch();
@@ -40,15 +41,15 @@ const Profile: React.FC<Props> = () => {
     } else dispatch(setProfile(id));
   };
 
-  const getAllData = () => {
+  const getAllData = async () => {
     getProfile();
-
-    dispatch(setLeaderboardBatting({ type: "exit_velocity" }));
-    dispatch(setLeaderboardPitching({ type: "pitch_velocity" }));
-    dispatch(setProfiles({ profiles_count: 10, offset: 0 }));
-    dispatch(getSchools(""));
-    dispatch(getTeams(""));
-    dispatch(getFacilities(""));
+    await dispatch(getSchools(""));
+    await dispatch(getTeams(""));
+    await dispatch(getFacilities(""));
+    await dispatch(setLeaderboardBatting({ type: "exit_velocity" }));
+    await dispatch(setLeaderboardPitching({ type: "pitch_velocity" }));
+    await dispatch(setProfiles({ profiles_count: 10, offset: 0 }));
+    await setSpinner(false);
   };
 
   useEffect(() => {
@@ -58,95 +59,103 @@ const Profile: React.FC<Props> = () => {
 
   return (
     <AppLayout>
-      <Container>
-        {editBtn === true ? (
-          <LeftPanel>
-            <EditProfile
-              toggleEditBtn={toggleEditBtn}
-              profile={profile}
-            ></EditProfile>
-          </LeftPanel>
-        ) : (
-          <LeftPanel>
-            <FeaturesProfile
-              toggleEditBtn={toggleEditBtn}
-              player={player}
-            ></FeaturesProfile>
-          </LeftPanel>
-        )}
+      {spinner ? (
+        <Spinner>
+          <SpinnerGif />
+        </Spinner>
+      ) : (
+        <Container>
+          {editBtn === true ? (
+            <LeftPanel>
+              <EditProfile
+                toggleEditBtn={toggleEditBtn}
+                profile={profile}
+              ></EditProfile>
+            </LeftPanel>
+          ) : (
+            <LeftPanel>
+              <FeaturesProfile
+                toggleEditBtn={toggleEditBtn}
+                player={player}
+              ></FeaturesProfile>
+            </LeftPanel>
+          )}
 
-        {profile?.first_name === null ? (
-          <MainContent>
-            <Appeal />
-          </MainContent>
-        ) : (
-          <MainContent>
-            <SummaryEvents>
-              <Summary>
-                <SumTitle>Top Pitching Values</SumTitle>
-                <SumRecord>
-                  <ProgressBar
-                    title="Fastball Velocity"
-                    value={
-                      player?.pitcher_summary[0]?.velocity?.toString() || "N/A"
-                    }
-                  />
-                  <ProgressBar
-                    title="Spin Rate"
-                    value={
-                      player?.pitcher_summary[0]?.spin_rate?.toString() || "N/A"
-                    }
-                  />
-                  <ProgressBar
-                    title="Pitch Movement"
-                    value={
-                      player?.pitcher_summary[0]?.horizontal_break?.toString() ||
-                      "N/A"
-                    }
-                  />
-                </SumRecord>
-              </Summary>
-              <Summary>
-                <SumTitle>Top Batting Values</SumTitle>
-                <SumRecord>
-                  <ProgressBar
-                    title="Exit Velocity"
-                    value={
-                      player?.batter_summary[0]?.exit_velocity?.toString() ||
-                      "N/A"
-                    }
-                  />
-                  <ProgressBar
-                    title="Carry Distance"
-                    value={
-                      player?.batter_summary[0]?.distance?.toString() || "N/A"
-                    }
-                  />
-                  <ProgressBar
-                    title="Launch Angle"
-                    value={
-                      player?.batter_summary[0]?.launch_angle?.toString() ||
-                      "N/A"
-                    }
-                  />
-                </SumRecord>
-              </Summary>
-              <RecentEvents>
-                <EvTitle>Recent Session Reports</EvTitle>
-                <EvData>No data currently linked to this profile</EvData>
-              </RecentEvents>
-            </SummaryEvents>
-            <InfoCard>
-              <TabBtnsWrapper>
-                <TabBtn isActive={activeTab} text="Batting"></TabBtn>
-                <TabBtn isActive={!activeTab} text="Session Reports"></TabBtn>
-                <TabBtn isActive={!activeTab} text="Comparison"></TabBtn>
-              </TabBtnsWrapper>
-              <InnerContent>There's no info yet!</InnerContent>
-            </InfoCard>
-          </MainContent>
-        )}
-      </Container>
+          {profile?.first_name === null ? (
+            <MainContent>
+              <Appeal />
+            </MainContent>
+          ) : (
+            <MainContent>
+              <SummaryEvents>
+                <Summary>
+                  <SumTitle>Top Pitching Values</SumTitle>
+                  <SumRecord>
+                    <ProgressBar
+                      title="Fastball Velocity"
+                      value={
+                        player?.pitcher_summary[0]?.velocity?.toString() ||
+                        "N/A"
+                      }
+                    />
+                    <ProgressBar
+                      title="Spin Rate"
+                      value={
+                        player?.pitcher_summary[0]?.spin_rate?.toString() ||
+                        "N/A"
+                      }
+                    />
+                    <ProgressBar
+                      title="Pitch Movement"
+                      value={
+                        player?.pitcher_summary[0]?.horizontal_break?.toString() ||
+                        "N/A"
+                      }
+                    />
+                  </SumRecord>
+                </Summary>
+                <Summary>
+                  <SumTitle>Top Batting Values</SumTitle>
+                  <SumRecord>
+                    <ProgressBar
+                      title="Exit Velocity"
+                      value={
+                        player?.batter_summary[0]?.exit_velocity?.toString() ||
+                        "N/A"
+                      }
+                    />
+                    <ProgressBar
+                      title="Carry Distance"
+                      value={
+                        player?.batter_summary[0]?.distance?.toString() || "N/A"
+                      }
+                    />
+                    <ProgressBar
+                      title="Launch Angle"
+                      value={
+                        player?.batter_summary[0]?.launch_angle?.toString() ||
+                        "N/A"
+                      }
+                    />
+                  </SumRecord>
+                </Summary>
+                <RecentEvents>
+                  <EvTitle>Recent Session Reports</EvTitle>
+                  <EvData>No data currently linked to this profile</EvData>
+                </RecentEvents>
+              </SummaryEvents>
+              <InfoCard>
+                <TabBtnsWrapper>
+                  <TabBtn isActive={activeTab} text="Batting"></TabBtn>
+                  <TabBtn isActive={!activeTab} text="Session Reports"></TabBtn>
+                  <TabBtn isActive={!activeTab} text="Comparison"></TabBtn>
+                </TabBtnsWrapper>
+                <InnerContent>There's no info yet!</InnerContent>
+              </InfoCard>
+            </MainContent>
+          )}
+        </Container>
+      )}
     </AppLayout>
   );
 };
@@ -155,6 +164,18 @@ const Container = styled.div`
   display: flex;
   width: 100%;
   height: 100%;
+`;
+const Spinner = styled(Container)`
+  justify-content: center;
+  align-items: center;
+  background: #fffeff;
+`;
+const SpinnerGif = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  background: url("https://media.giphy.com/media/feN0YJbVs0fwA/giphy.gif")
+    no-repeat center;
 `;
 const MainContent = styled.main`
   display: flex;
